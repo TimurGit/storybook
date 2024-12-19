4c81e44fbca484475646c5cbd0994a316ae553bc
Timur Mustafin <mustafintf@eruditor-group.com>
2024-12-19 22:27:50 +0300

    ads tailwind

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,13 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)",
+'../tailwind.config.js'],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "./local-preset.js",
+        'storybook-addon-tailwind-autodocs', // 👈 register the addon here
   ],
   framework: {
     name: "@storybook/react-vite",
